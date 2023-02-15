19eadc2ec59d6d87a53ae46e58415932cb25b37b
Chase Hainey <chaseh@replicated.com>
2023-02-15 22:10:18 +0000

    Own preflight, updated the  cleanup.

--- tasks.sh
+++ tasks.sh
@@ -4,16 +4,43 @@ os=$(uname -s)
 arch=$(uname -m)
 preflight_current_version="v0.57.1"
 url="https://github.com/replicatedhq/troubleshoot/releases/download"
-preflight_yaml="https://raw.githubusercontent.com/replicatedhq/troubleshoot/main/examples/preflight/sample-preflight.yaml"
+preflight_yaml="https://raw.githubusercontent.com/chasehainey/sandbox/main/preflightyaml"
 supportbundle_yaml="https://raw.githubusercontent.com/chasehainey/sandbox/main/support-bundle.yaml"
+
+cleanup(){
+	if [ -f /tmp/LICENSE ]
+	then
+		rm /tmp/LICENSE
+	fi
+	if [ -f /tmp/README.md ]
+	then
+		rm /tmp/README.md
+	fi
+	if [ -f /tmp/key.pub ]
+	then
+		rm /tmp/key.pub
+	fi
+	if [ -f /tmp/troubleshoot-sbom.tgz ]
+	then
+		rm /tmp/troubleshoot-sbom.tgz*
+	fi
+	if [ -f /tmp/preflight ]
+	then
+		rm /tmp/preflight*
+	fi
+	if [ -f /tmp/support-bundle ]
+	then
+		rm /tmp/support-bundle*
+	fi
+}
 
 preflight_linux_os_x86_64(){
 	echo "Replicated Preflight loading for Linux x86_64..."
 	wget -q -P /tmp $url/$preflight_current_version/preflight_linux_amd64.tar.gz
 	tar zxf /tmp/preflight_linux_amd64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
-	/tmp/preflight /tmp/sample-preflight.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+	/tmp/preflight /tmp/preflight.yaml
+	cleanup
 }
 
 preflight_linux_os_arm64(){
@@ -21,8 +48,8 @@ preflight_linux_os_arm64(){
 	wget -q -P /tmp $url/$preflight_current_version/preflight_linux_arm64.tar.gz
 	tar zxf /tmp/preflight_linux_arm64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
-	/tmp/preflight /tmp/sample-preflight.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+	/tmp/preflight /tmp/preflight.yaml
+	cleanup
 }
 
 preflight_macos_arm64(){
@@ -30,8 +57,8 @@ preflight_macos_arm64(){
 	wget -q -P /tmp $url/$preflight_current_version/preflight_darwin_arm64.tar.gz
 	tar zxf /tmp/preflight_darwin_arm64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
-	/tmp/preflight /tmp/sample-preflight.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+	/tmp/preflight /tmp/preflight.yaml
+	cleanup
 }
 
 preflight_macos_x86_64(){
@@ -39,8 +66,8 @@ preflight_macos_x86_64(){
 	wget -q -P /tmp $url/$preflight_current_version/preflight_darwin_amd64.tar.gz
 	tar zxf /tmp/preflight_darwin_amd64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
-	/tmp/preflight /tmp/sample-preflight.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+	/tmp/preflight /tmp/preflight.yaml
+	cleanup
 }
 
 supportbundle_linux_os_x86_64(){
@@ -49,7 +76,7 @@ supportbundle_linux_os_x86_64(){
 	tar zxf /tmp/support-bundle_linux_amd64.tar.gz -C /tmp
 	wget -q -P /tmp $supportbundle_yaml
 	/tmp/support-bundle /tmp/support-bundle.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/support-bundle*
+	cleanup
 }
 
 supportbundle_linux_os_arm64(){
@@ -58,7 +85,7 @@ supportbundle_linux_os_arm64(){
 	tar zxf /tmp/support-bundle_linux_arm64.tar.gz -C /tmp
 	wget -q -P /tmp $supportbundle_yaml
 	/tmp/support-bundle /tmp/support-bundle.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/support-bundle*
+	cleanup
 }
 
 supportbundle_macos_arm64(){
@@ -67,7 +94,7 @@ supportbundle_macos_arm64(){
 	tar zxf /tmp/support-bundle_darwin_arm64.tar.gz -C /tmp
 	wget -q -P /tmp $supportbundle_yaml
 	/tmp/support-bundle /tmp/support-bundle.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/support-bundle*
+	cleanup
 }
 
 supportbundle_macos_x86_64(){
@@ -76,7 +103,7 @@ supportbundle_macos_x86_64(){
 	tar zxf /tmp/support-bundle_darwin_amd64.tar.gz -C /tmp
 	wget -q -P /tmp $supportbundle_yaml
 	/tmp/support-bundle /tmp/support-bundle.yaml
-	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/support-bundle*
+	cleanup
 }
 
 preflight(){
