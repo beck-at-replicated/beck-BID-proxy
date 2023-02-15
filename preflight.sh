8bd9116ab95317b6366358992bc4fc4b9c89f388
Chase Hainey <chaseh@replicated.com>
2023-02-15 20:42:21 +0000

    Pushing support-bundle yaml.

--- preflight.sh
+++ preflight.sh
@@ -2,56 +2,127 @@
 
 os=$(uname -s)
 arch=$(uname -m)
-current_version="v0.57.1"
-url="https://github.com/replicatedhq/troubleshoot/releases/download"
+preflight_current_version="v0.57.1"
+preflight_url="https://github.com/replicatedhq/troubleshoot/releases/download"
 preflight_yaml="https://raw.githubusercontent.com/replicatedhq/troubleshoot/main/examples/preflight/sample-preflight.yaml"
 
-linux_os_x86_64(){
+preflight_linux_os_x86_64(){
 	echo "Replicated Preflight loading for Linux x86_64..."
-	wget -q -P /tmp $url/$current_version/preflight_linux_amd64.tar.gz
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_linux_amd64.tar.gz
 	tar zxf /tmp/preflight_linux_amd64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
 	/tmp/preflight /tmp/sample-preflight.yaml
 	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
 }
 
-linux_os_arm64(){
+preflight_linux_os_arm64(){
 	echo "Replicated Preflight loading for Linux arm64..."
-	wget -q -P /tmp $url/$current_version/preflight_linux_arm64.tar.gz
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_linux_arm64.tar.gz
 	tar zxf /tmp/preflight_linux_arm64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
 	/tmp/preflight /tmp/sample-preflight.yaml
 	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
 }
 
-macos_arm64(){
+preflight_macos_arm64(){
 	echo "Replicated Preflight loading for macOS arm64..."
-	wget -q -P /tmp $url/$current_version/preflight_darwin_arm64.tar.gz
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_darwin_arm64.tar.gz
 	tar zxf /tmp/preflight_darwin_arm64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
 	/tmp/preflight /tmp/sample-preflight.yaml
 	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
 }
 
-macos_x86_64(){
+preflight_macos_x86_64(){
 	echo "Replicated Preflight loading for macOS x86_64..."
-	wget -q -P /tmp $url/$current_version/preflight_darwin_amd64.tar.gz
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_darwin_amd64.tar.gz
 	tar zxf /tmp/preflight_darwin_amd64.tar.gz -C /tmp
 	wget -q -P /tmp $preflight_yaml
 	/tmp/preflight /tmp/sample-preflight.yaml
 	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
 }
 
-if [ $os == "Linux" ] && [ $arch == "x86_64" ]
+supportbundle_linux_os_x86_64(){
+	echo "Replicated Support Bundle loading for Linux x86_64..."
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_linux_amd64.tar.gz
+	tar zxf /tmp/preflight_linux_amd64.tar.gz -C /tmp
+	wget -q -P /tmp $preflight_yaml
+	/tmp/preflight /tmp/sample-preflight.yaml
+	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+}
+
+supportbundle_linux_os_arm64(){
+	echo "Replicated Support Bundle loading for Linux arm64..."
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_linux_arm64.tar.gz
+	tar zxf /tmp/preflight_linux_arm64.tar.gz -C /tmp
+	wget -q -P /tmp $preflight_yaml
+	/tmp/preflight /tmp/sample-preflight.yaml
+	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+}
+
+supportbundle_macos_arm64(){
+	echo "Replicated Support Bundle loading for macOS arm64..."
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_darwin_arm64.tar.gz
+	tar zxf /tmp/preflight_darwin_arm64.tar.gz -C /tmp
+	wget -q -P /tmp $preflight_yaml
+	/tmp/preflight /tmp/sample-preflight.yaml
+	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+}
+
+supportbundle_macos_x86_64(){
+	echo "Replicated Support Bundle loading for macOS x86_64..."
+	wget -q -P /tmp $preflight_url/$preflight_current_version/preflight_darwin_amd64.tar.gz
+	tar zxf /tmp/preflight_darwin_amd64.tar.gz -C /tmp
+	wget -q -P /tmp $preflight_yaml
+	/tmp/preflight /tmp/sample-preflight.yaml
+	rm /tmp/LICENSE /tmp/README.md /tmp/key.pub /tmp/troubleshoot-sbom.tgz* /tmp/preflight* /tmp/sample-preflight*
+}
+
+preflight(){
+	if [ $os == "Linux" ] && [ $arch == "x86_64" ]
+	then
+		preflight_linux_os_x86_64
+	elif [ $os == "Linux" ] && ([ $arch == "arm64"] || [ $arch == "aarch64" ])
+	then
+		preflight_linux_os_arm64
+	elif [ $os == "Darwin" ] && ([ $arch == "arm64" ] || [ $arch == "aarch64" ])
+	then
+		preflight_macos_arm64
+	elif [ $os == "Darwin" ] && [ $arch == "x86_64" ]
+	then
+		preflight_macos_x86_64
+	fi
+}
+
+supportbundle(){
+	if [ $os == "Linux" ] && [ $arch == "x86_64" ]
+	then
+		supportbundle_linux_os_x86_64
+	elif [ $os == "Linux" ] && ([ $arch == "arm64"] || [ $arch == "aarch64" ])
+	then
+		supportbundle_linux_os_arm64
+	elif [ $os == "Darwin" ] && ([ $arch == "arm64" ] || [ $arch == "aarch64" ])
+	then
+		supportbundle_macos_arm64
+	elif [ $os == "Darwin" ] && [ $arch == "x86_64" ]
+	then
+		supportbundle_macos_x86_64
+	fi
+}
+
+while getopts s: flag
+do
+	case "${flag}" in
+		s) task=${OPTARG};;
+	esac
+done
+
+if [ $task == "preflight" ]
 then
-	linux_os_x86_64
-elif [ $os == "Linux" ] && ([ $arch == "arm64"] || [ $arch == "aarch64" ])
+	preflight
+elif [ $task == "supportbundle" ] || [ $task == "support-bundle" ]
 then
-	linux_os_arm64
-elif [ $os == "Darwin" ] && ([ $arch == "arm64" ] || [ $arch == "aarch64" ])
-then
-	macos_arm64
-elif [ $os == "Darwin" ] && [ $arch == "x86_64" ]
-then
-	macos_x86_64
+	supportbundle
+else
+	echo "Command not recognized."
 fi
